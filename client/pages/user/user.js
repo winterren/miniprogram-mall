25e07d4247f1dc69a8ff57181988a7e707d500d9
winterren <winterren@msn.com>
2018-12-08 13:12:10 +0800

    update

--- client/pages/user/user.js
+++ client/pages/user/user.js
@@ -1,3 +1,5 @@
+const qcloud = require('../../vendor/wafer2-client-sdk/index.js');
+const config = require('../../config.js');
 // pages/user/user.js
 Page({
 
@@ -6,7 +8,7 @@ Page({
    */
   data: {
     userInfo: {
-      nickName: "优达学城",
+      nickName: "未登录",
       avatarUrl: "", // 头像 URL 地址
     }, // 虚拟数据
   },
@@ -15,8 +17,99 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.checkSession({
+      success: ({ userInfo }) => {
+        this.setData({
+          userInfo: userInfo
+        })
+      },
+      error: () => { }
+    })
+    qcloud.setLoginUrl(config.service.loginUrl);
+    qcloud.login({
+      success: res=>{
+        console.log('suc');
+        console.log(res);
+      },
+      fail: res=>{
+        console.log('fail');
+        console.log(res);
+      }
+    })
+    
   },
+  checkSession({ success, error }) {
+    wx.checkSession({
+      success: () => {
+        this.getUserInfo({ success, error })
+      },
+      fail: () => {
+        error && error()
+      }
+    })
+  },
+  doCloudLogin(suc,err){
+    qcloud.login({
+      success: res=>{
+        if(res){
+          let userInfo =res;
+          suc && suc({
+            userInfo
+          })
+        }else{
+          // 如不是首次登陆，不会返回用户信息，请求用户信息借口获取
+          getUserInfo({success,error})
+        }
+      }
+    })
+  },
+  getUserInfo({suc,err}){
+    qcloud.request({
+      url:config.service.requestUrl,
+      login:true,
+      success:result=>{
+        let data = result.data
+        if (!data.data){
+          let userInfo = data.data
+          suc && suc({
+            userInfo
+          }) 
+        } else {
+          err && err
+        }
+      },
+      fail:()=>{
+        err&&err()
+      }
+    })
+  },
+  checkSession({ success, error }) {
+    wx.checkSession({
+      success: () => {
+        this.getUserInfo({ success, error })
+      },
+      fail: () => {
+        error && error()
+      }
+    })
+  },
+  onTapLogin: function () {
+    qcloud.setLoginUrl(config.service.loginUrl)
+    qcloud.login({
+      success: result => {
+        console.log('success')
+        console.log(result)
+        this.setData({
+          userInfo: result
+        })
+      },
+      fail: result => {
+        console.log('fail')
+        console.log(result)
+      }
+    })
+  },
+
 
   /**
    * 生命周期函数--监听页面初次渲染完成
